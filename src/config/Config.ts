d69d325b145e270b929078dad17e4ee1d12694f3
Oblitus <piotr5jurczak@gmail.com>
2024-09-08 12:12:45 +0200

    add global config

---
+++ src/config/Config.ts
@@ -0,0 +1,6 @@
+export const REGISTER_IDLE = false;
+export const REFRESH_TIME = 250;
+
+export const COMMAND_START = "workingtimetracker.start";
+export const COMMAND_STOP = "workingtimetracker.end";
+export const COMMAND_IDLE = "workingtimetracker.idle";
