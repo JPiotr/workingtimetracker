1b5444d2d52db9dd3bed3d726bc0f9f4b74c2aba
Oblitus <piotr5jurczak@gmail.com>
2024-10-14 08:58:06 +0200

    update config file

--- src/config/Config.ts
+++ src/config/Config.ts
@@ -1,6 +1,5 @@
-export const REGISTER_IDLE = false;
-export const REFRESH_TIME = 250;
-
 export const COMMAND_START = "workingtimetracker.start";
 export const COMMAND_STOP = "workingtimetracker.end";
 export const COMMAND_IDLE = "workingtimetracker.idle";
+export const COMMAND_TEST = "workingtimetracker.test";
+export const COMMAND_TEST1 = "workingtimetracker.test1";
