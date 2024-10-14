f8fa7614f1ccd7426b35baaacf7dd1fea94c306f
Oblitus <piotr5jurczak@gmail.com>
2024-10-14 09:43:44 +0200

    add session model

--- src/core/Sessions/ISessionInfo.ts
+++ src/core/Sessions/ISessionInfo.ts
@@ -1,6 +1,9 @@
+import { ISessionDurationInfo } from "./ISessionDurationInfo";
 import { SessionState } from "./SessionState";
 
 export interface ISessionInfo {
+    id : string;
     duration: number;
     state: SessionState;
+    durations: ISessionDurationInfo[];
 }
