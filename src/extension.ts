3fd0781fd1c881c1269a2c8520b84caa4b625bd6
Oblitus <piotr5jurczak@gmail.com>
2024-10-14 14:31:32 +0200

    update extension main file

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,116 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import { COMMAND_IDLE, COMMAND_START, COMMAND_STOP, COMMAND_TEST, COMMAND_TEST1 } from './config/Config';
+import { ActionType } from "./core/ActionType";
+import { interval, Subscription } from "rxjs";
+import { SessionManager } from './core/Sessions/SessionManager';
+import { SessionState } from './core/Sessions/SessionState';
+import { convertTimeToString } from './static/Utils';
+import { DataStorageManager } from './core/Data/DataStorageManager';
+import { BehaviorDetector } from './core/Behavior/BehaviorDetector';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
+const config = vscode.workspace.getConfiguration('workingtimetracker');
+const ticks = interval(config.innerSessions.uiRefreshTime);
+const autoSave = interval(config.innerSessions.autoSaveTime);
+const idle = interval(config.innerSessions.idleTime);
+let subscription = Subscription.EMPTY;
+let autoSaveSubsciption = Subscription.EMPTY;
+let idleSub = Subscription.EMPTY;
+let statusBarItem : vscode.StatusBarItem;
+
+const ICON_STARTED = '$(debug-start)';
+const ICON_STOPPED = '$(debug-stop)';
+const ICON_PAUSED = '$(debug-pause)';
+let newly : boolean = true;
+
 export function activate(context: vscode.ExtensionContext) {
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "workingtimetracker" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('workingtimetracker.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from WorkingTimeTracker!');
+	context.subscriptions.push(
+		vscode.commands.registerCommand(COMMAND_START, ()=>{
+			SessionManager.getInstance().menageSession(ActionType.Codding);
+			newly = false;
+		}),
+		vscode.commands.registerCommand(COMMAND_STOP, ()=>{
+			SessionManager.getInstance().menageSession(ActionType.Stop);
+			newly = false;
+		}),
+		vscode.commands.registerCommand(COMMAND_IDLE, ()=>{
+			SessionManager.getInstance().menageSession(ActionType.Idle);
+			newly = false;
+		}),
+		vscode.commands.registerCommand(COMMAND_TEST, ()=>{
+			DataStorageManager.getInstance().saveData();
+		}),
+		vscode.commands.registerCommand(COMMAND_TEST1, ()=>{
+			DataStorageManager.getInstance().loadData();
+		}),
+		...BehaviorDetector.getInstance().detectCodding(),
+		...BehaviorDetector.getInstance().detectDebuging(),
+		...BehaviorDetector.getInstance().detectDocumenting(),
+		...BehaviorDetector.getInstance().detectBuilding(),
+		...BehaviorDetector.getInstance().detectIdle()
+	);
+	
+	statusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Right, 500);
+	statusBarItem.show();
+	context.subscriptions.push(statusBarItem);
+	subscription = ticks.subscribe(()=>{
+		updateStatusBarItem();
 	});
+	idleSub = idle.subscribe(()=>{
+		if(Date.now() - BehaviorDetector.getInstance().lastActive > config.innerSessions.idleTime){
+			BehaviorDetector.getInstance().idleUser();
+		}
+	});
+	updateStatusBarItem();
+	
+	DataStorageManager.getInstance().loadData();
+	autoSaveSubsciption = autoSave.subscribe(()=>{
+		if(config.innerSessions.autoSave && !newly){
+			DataStorageManager.getInstance().saveData();
+		}
+	});
+	
 
-	context.subscriptions.push(disposable);
+}
+
+function updateStatusBarItem() : void {
+	if(BehaviorDetector.getInstance().isDetectedNewAction()){
+		newly = false;
+	}
+	if(newly){
+		statusBarItem.text = `${ICON_STOPPED} Not Started Time: ${convertTimeToString(0)}`;
+		statusBarItem.command = COMMAND_START;
+		return; 
+	}
+	let sessionMenager = SessionManager.getInstance();
+	const sessionDataRow = sessionMenager.getSessionInfo();
+
+	const duration = sessionDataRow.sessionInfo.duration;
+	const icon = sessionDataRow.sessionInfo.state === SessionState.Ongoing ? ICON_STARTED : 
+		sessionDataRow.sessionInfo.state === SessionState.Idle ? ICON_PAUSED : ICON_STOPPED;
+	const action = getState(sessionDataRow.actionType);
+
+	statusBarItem.text = `${icon} ${action} | Time: ${convertTimeToString(duration)}`;
+	statusBarItem.command = sessionDataRow.sessionInfo.state === SessionState.Ongoing ? COMMAND_STOP : COMMAND_START; 
+}
+
+function getState(state : ActionType) : string | undefined{
+	switch(state){
+		case ActionType.Idle:  return "Idle       ";
+		case ActionType.Codding:  return "Codding    ";
+		case ActionType.Debugging:  return "Debugging  ";
+		case ActionType.Documenting:  return "Documenting";
+		case ActionType.Testing:  return "Testing    ";
+		case ActionType.Stop:  return "Stop       ";
+		case ActionType.Building:  return "Building   ";
+		default: return "Undefined  ";
+	}
 }
 
 // This method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() {
+	SessionManager.getInstance().menageSession(ActionType.Stop);
+	DataStorageManager.getInstance().saveData();
+	subscription.unsubscribe();
+	autoSaveSubsciption.unsubscribe();
+	idleSub.unsubscribe();
+}
