b085fc1673a7b6dc9dc1bfa09f1d64ee199bfd70
Oblitus <piotr5jurczak@gmail.com>
2024-10-20 14:14:52 +0200

    add some documentation, prettier code, eleminate bug with seconds not converting from string

--- src/static/Utils.ts
+++ src/static/Utils.ts
@@ -1,28 +1,45 @@
+/**
+ * Helps convert string to time in ms
+ * @param value - Time in format "00:00:00"
+ * @returns time in ms
+ */
+export function convertStringToTime(value: string): number {
+  if (value[0] === "-") {
+    throw Error("Cannot convert negative time string!");
+  }
+  const data = value.split(":");
+  const h = Number(data[0]) * 60 * 60 * 1000;
+  const m = Number(data[1]) * 60 * 1000;
+  const s = Number(data[2]) * 1000;
 
-export function convertStringToTime(value : string) : number{
-    let data = value.split(':');
-    let h = Number(data[0]) * 60 * 60 * 1000;
-    let m = Number(data[1]) * 60 * 1000;
-
-    return h + m ;
+  return h + m + s;
 }
 
 /**
- * 
- * @param value - Time to convert. 
+ * Helps convert ms to time string
+ * @param value - Time to convert.
  * @returns time in "00:00:00" format
  */
-export function convertTimeToString(value : number) : string {
-    let seconds = Math.floor(value / 1000);
-    let minutes = Math.floor(seconds / 60);
-    let hours = Math.floor(minutes / 60);
+export function convertTimeToString(value: number): string {
+  if (value < 0) {
+    throw Error("Cannot convert negative time to string!");
+  }
+  let seconds = Math.floor(value / 1000);
+  let minutes = Math.floor(seconds / 60);
+  const hours = Math.floor(minutes / 60);
 
-    seconds = seconds % 60;
-    minutes = minutes % 60;
+  seconds = seconds % 60;
+  minutes = minutes % 60;
 
-    return padTo2Digits(hours) + ":" + padTo2Digits(minutes) + ":" + padTo2Digits(seconds);
+  return (
+    padTo2Digits(hours) +
+    ":" +
+    padTo2Digits(minutes) +
+    ":" +
+    padTo2Digits(seconds)
+  );
 }
 
-function padTo2Digits(num : number) : string {
-    return num.toString().padStart(2, '0');
+function padTo2Digits(num: number): string {
+  return num.toString().padStart(2, "0");
 }
