2e0b0ddb1906416b4dc2d18012d359470e2d9f88
Oblitus <piotr5jurczak@gmail.com>
2024-10-20 14:21:08 +0200

    add tests for session duration

--- src/test/Sessions/sessionDuration.test.ts
+++ src/test/Sessions/sessionDuration.test.ts
@@ -1,19 +1,26 @@
 import assert from "assert";
 import { SessionDuration } from "../../core/Sessions/SessionDuration";
 
-let duration : SessionDuration;
-suiteSetup('Tests Setup',()=>{
-    duration = new SessionDuration();
+suite(`SessionDuration`, () => {
+  test(`Should duration start`, () => {
+    const duration: SessionDuration = new SessionDuration();
     duration.start();
-});
-suite('Session Duration Tests', ()=>{
-    test('Duration not ended',()=>{
-        assert.strictEqual(duration.getDuration(),0);
-    });
-    test('Duration ended', ()=>{
-        setTimeout(()=>{
-            duration.end();
-            assert.strictEqual(duration.getDuration() > 1000, true );
-        },1000);
-    });
+    assert.equal(duration.getInfo().id !== undefined, true);
+  });
+  test(`Should duration end`, () => {
+    const duration: SessionDuration = new SessionDuration();
+    duration.start();
+    setTimeout(() => {
+      duration.end();
+      assert.equal(duration.getDuration() > 1000, true);
+    }, 1000);
+  });
+  test(`Should return current duration`, () => {
+    const duration: SessionDuration = new SessionDuration();
+    duration.start();
+    setTimeout(() => {
+      assert.equal(duration.getCurrentDuration() < 1000, true);
+      duration.end();
+    }, 1000);
+  });
 });
