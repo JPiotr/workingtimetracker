4972758ce67b8f931b7fa40494a7cf04da2016a9
Oblitus <piotr5jurczak@gmail.com>
2024-10-20 20:03:00 +0200

    delete unused code

--- src/config/Config.ts
+++ src/config/Config.ts
@@ -2,4 +2,3 @@ export const COMMAND_START = "workingtimetracker.start";
 export const COMMAND_STOP = "workingtimetracker.end";
 export const COMMAND_IDLE = "workingtimetracker.idle";
 export const COMMAND_SAVE = "workingtimetracker.save";
-// export const COMMAND_CHANGE_AUTOSAVE = "workingtimetracker.autoSave";
