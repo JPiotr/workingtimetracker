104894ed110ca86321656b65e4f98abb9d1f4609
Oblitus <piotr5jurczak@gmail.com>
2024-10-20 14:19:55 +0200

    prettier code, delete unused method

--- src/core/Node.ts
+++ src/core/Node.ts
@@ -1,19 +1,9 @@
-
 export class Node<T> {
+  current: T;
+  previous: Node<T> | null;
 
-    current: T;
-    previous: Node<T> | null;
-
-    constructor(current: T, previous: Node<T> | null = null) {
-        this.previous = previous;
-        this.current = current;
-    }
-
-    public getFirst(): T {
-        let node: Node<T> = this;
-        while (node.previous !== null) {
-            node = node.previous;
-        }
-        return node.current;
-    }
+  constructor(current: T, previous: Node<T> | null = null) {
+    this.previous = previous;
+    this.current = current;
+  }
 }
