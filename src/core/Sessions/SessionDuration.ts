65ae3142378a84caa125ea8e0bb8e559827d5124
Oblitus <piotr5jurczak@gmail.com>
2024-10-23 17:44:33 +0200

    add storing session state when duration is started

--- src/core/Sessions/SessionDuration.ts
+++ src/core/Sessions/SessionDuration.ts
@@ -1,12 +1,15 @@
 import { ISessionDurationInfo } from "./ISessionDurationInfo";
+import { SessionState } from "./SessionState";
 
 export class SessionDuration {
   private _id!: string;
+  private _state!: SessionState;
   private _begin!: number;
   private _end!: number;
   private _duration: number = 0;
 
-  public start(): void {
+  public start(state: SessionState): void {
+    this._state = state;
     this._id = crypto.randomUUID();
     this._begin = Date.now();
     this._end = this._begin;
@@ -22,12 +25,15 @@ export class SessionDuration {
   }
 
   public getCurrentDuration(): number {
-    return this._duration !== 0 ? this._duration : Date.now() - this._begin;
+    return this._duration !== 0 ? 
+      this._duration : 
+      Date.now() - this._begin;
   }
 
   public getInfo(): ISessionDurationInfo {
     return {
       id: this._id,
+      state : this._state,
       begin: this._begin,
       end: this._end,
       duration: this._duration,
