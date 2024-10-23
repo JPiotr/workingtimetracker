293d635dd435b8f1bbf749605764e33904d7e137
Oblitus <piotr5jurczak@gmail.com>
2024-10-23 21:56:58 +0200

    Update tests

--- src/test/Sessions/sessionDuration.test.ts
+++ src/test/Sessions/sessionDuration.test.ts
@@ -1,26 +1,32 @@
 import assert from "assert";
 import { SessionDuration } from "../../core/Sessions/SessionDuration";
+import { SessionState } from "../../core/Sessions/SessionState";
+import { beforeEach } from "mocha";
 
 suite(`SessionDuration`, () => {
+  let duration: SessionDuration;
+  beforeEach(() => {
+    duration = new SessionDuration();
+    duration.start(SessionState.Ongoing);
+  });
   test(`Should duration start`, () => {
-    const duration: SessionDuration = new SessionDuration();
-    duration.start();
     assert.equal(duration.getInfo().id !== undefined, true);
+    duration.end();
   });
   test(`Should duration end`, () => {
-    const duration: SessionDuration = new SessionDuration();
-    duration.start();
     setTimeout(() => {
       duration.end();
       assert.equal(duration.getDuration() > 1000, true);
     }, 1000);
   });
   test(`Should return current duration`, () => {
-    const duration: SessionDuration = new SessionDuration();
-    duration.start();
     setTimeout(() => {
       assert.equal(duration.getCurrentDuration() < 1000, true);
       duration.end();
     }, 1000);
+  });
+  test(`Should register duration state`, () => {
+    duration.end();
+    assert.equal(duration.getInfo().state, SessionState.Ongoing);
   });
 });
