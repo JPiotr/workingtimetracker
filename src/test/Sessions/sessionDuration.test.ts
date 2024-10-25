9a54db350279d5a2c2c93986320fb9f9d64e3c43
Oblitus <piotr5jurczak@gmail.com>
2024-10-25 12:10:17 +0200

    fix wrong tests

--- src/test/Sessions/sessionDuration.test.ts
+++ src/test/Sessions/sessionDuration.test.ts
@@ -16,32 +16,35 @@ suite(`SessionDuration`, () => {
   test(`Should not register duration before end`, ()=>{
     assert.equal(duration.getDuration(),0);
   });
-  test(`Should duration end`, () => {
-    setTimeout(() => {
-      duration.end();
-      assert.equal(duration.getDuration() > 1000, true);
-    }, 1000);
+  test(`Should duration end`, async () => {
+    await new Promise((resolve) => {
+      setTimeout(resolve, 500);
+    });
+    duration.end();
+    assert.equal(duration.getDuration() >= 500, true);
   });
-  test(`Should return current duration`, () => {
-    setTimeout(() => {
-      assert.equal(duration.getCurrentDuration() < 1000, true);
-      duration.end();
-    }, 1000);
+  test(`Should return current duration`, async () => {
+    await new Promise((resolve) => {
+      setTimeout(resolve, 500);
+    });
+    assert.equal(duration.getCurrentDuration() > 500, true);
+    duration.end();
   });
   test(`Should register duration state`, () => {
     duration.end();
     assert.equal(duration.getInfo().state, SessionState.Ongoing);
   });
-  test(`Should return all information`, ()=>{
-    setTimeout(()=>{
-      duration.end();
-      const info = duration.getInfo();
-      assert.notEqual(info, undefined);
-      assert.notEqual(info, null);
-      assert.notEqual(info.id,"");
-      assert.equal(info.duration > 1000, true);
-      assert.equal(info.state, SessionState.Ongoing);
-      assert.equal(info.begin > info.end, true);
-    },1000);
+  test(`Should return all information`, async ()=>{
+    await new Promise((resolve) => {
+      setTimeout(resolve, 500);
+    });
+    duration.end();
+    const info = duration.getInfo();
+    assert.notEqual(info, undefined);
+    assert.notEqual(info, null);
+    assert.notEqual(info.id,"");
+    assert.equal(info.duration > 500, true);
+    assert.equal(info.state, SessionState.Ongoing);
+    assert.equal(info.begin < info.end, true);
   })
 });
