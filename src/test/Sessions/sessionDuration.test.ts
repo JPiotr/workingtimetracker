7e4f026a137b431de201ba930b3b8d0bb10fe672
Oblitus <piotr5jurczak@gmail.com>
2024-10-25 12:12:29 +0200

    prittier code

--- src/test/Sessions/sessionDuration.test.ts
+++ src/test/Sessions/sessionDuration.test.ts
@@ -13,8 +13,8 @@ suite(`SessionDuration`, () => {
     assert.equal(duration.getInfo().id !== undefined, true);
     duration.end();
   });
-  test(`Should not register duration before end`, ()=>{
-    assert.equal(duration.getDuration(),0);
+  test(`Should not register duration before end`, () => {
+    assert.equal(duration.getDuration(), 0);
   });
   test(`Should duration end`, async () => {
     await new Promise((resolve) => {
@@ -34,7 +34,7 @@ suite(`SessionDuration`, () => {
     duration.end();
     assert.equal(duration.getInfo().state, SessionState.Ongoing);
   });
-  test(`Should return all information`, async ()=>{
+  test(`Should return all information`, async () => {
     await new Promise((resolve) => {
       setTimeout(resolve, 500);
     });
@@ -42,9 +42,9 @@ suite(`SessionDuration`, () => {
     const info = duration.getInfo();
     assert.notEqual(info, undefined);
     assert.notEqual(info, null);
-    assert.notEqual(info.id,"");
+    assert.notEqual(info.id, "");
     assert.equal(info.duration > 500, true);
     assert.equal(info.state, SessionState.Ongoing);
     assert.equal(info.begin < info.end, true);
-  })
+  });
 });
